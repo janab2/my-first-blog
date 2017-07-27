486deea4ad9d86cc528bac431588a99691d37cfc
jana <jana.blaga@gmail.com>
2017-07-27 21:41:24 +0200

    Pokus č. 2

--- blog/models.py
+++ blog/models.py
@@ -5,14 +5,12 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	created_date = models.DateTimeField(
-		blank=True, null=True)
+	created_date = models.DateTimeField(blank=True, null=True)
+	published_date = models.DateTimeField(blank=True, null=True)
 
-def publish(self):
-	self.published_date = timezone.now()
-	self.save()
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
 
-def __str__(self):
-	return self.title
-	
-# Create your models here.
+	def __str__(self):
+		return self.title
